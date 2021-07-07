694d01d338da329d5b3195a773f2f735c3b976e4
Ivan Milincic <kreativan@outlook.com>
2021-07-07 23:17:54 +0200

    update pw image

--- dist/_nuxt/static/1625683144/manifest.js
+++
@@ -1,1 +0,0 @@
-__NUXT_JSONP__("manifest.js", {routes:["\u002F","\u002Fhire-me","\u002Fabout","\u002Fportfolio","\u002Fservices\u002Fcustom-websites","\u002Fservices\u002Fweb-development","\u002Fservices\u002Fstatic-websites","\u002Fservices\u002Foutsourcing","\u002Fservices\u002Fprocesswire"]})
